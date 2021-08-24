1137394c2bfb017961dfd4b09fdbe240ce8469ac
ahajcruz <gammaaharon23@gmail.com>
2021-08-24 16:25:40 -0500

    Se agrego la funcionalidad de ingresar persona a la base de datos

--- proyectoAPI/models.py
+++ proyectoAPI/models.py
@@ -2,14 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
+    idPersona=models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50)
     cedula = models.CharField(max_length=10)
     correo = models.CharField(max_length=30)
     direccion = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre
 class Tienda(models.Model):
     nombre=models.CharField(max_length=50)
     direccion=models.CharField(max_length=50)
-    dueño=models.ForeignKey(Persona,on_delete=models.CASCADE)
+    idPersona=models.ForeignKey(Persona,on_delete=models.CASCADE)
 class Producto(models.Model):
     nombre=models.CharField(max_length=50)
     precio=models.FloatField()
--- proyectoAPI/views.py
+++ proyectoAPI/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .forms import PersonaForm
 # Create your views here.
+
+def ingreso(request):
+	data={
+		'form':PersonaForm()
+	}
+	if request.method =='POST':
+		formulario=PersonaForm(data=request.POST)
+		if formulario.is_valid():
+			formulario.save()
+		else:
+			data["form"]=formulario
+	return render(request,'proyectoAPI/ingreso.html',data)
