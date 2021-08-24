d5f1a51af132c82b1a9f867c44db4f206fbbae00
Ricardo <[Your>
2021-08-24 12:38:30 -0500

    modelo persona

--- proyectoAPI/models.py
+++ proyectoAPI/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=50)
+    cedula = models.CharField(max_length=10)
+    correo = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=50)
 class Tienda(models.Model):
     nombre=models.CharField(max_length=50)
     direccion=models.CharField(max_length=50)
+    dueño=models.ForeignKey(Persona,on_delete=models.CASCADE)
 class Producto(models.Model):
     nombre=models.CharField(max_length=50)
     precio=models.FloatField()
@@ -12,6 +18,8 @@ class Producto(models.Model):
     tipo=models.CharField(max_length=50)
     tienda=models.ForeignKey(Tienda,on_delete=models.CASCADE)
     id=models.AutoField(primary_key=True)
+
+
 
     def __str__(self):
         return self.nombre
